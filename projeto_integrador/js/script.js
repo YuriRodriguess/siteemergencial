ba1295def243ca3eb6ebcf6199e35611a70c1440
YuriRodriguess <72237751+YuriRodriguess@users.noreply.github.com>
2020-10-14 08:49:38 -0300

    Add files via upload

---
+++ projeto_integrador/js/script.js
@@ -0,0 +1,12 @@
+function alerta() {
+    alert('Não sei onde tu usaria esse alerta, mas é um exemplo');
+}
+
+function cor (x) {
+    x.style.color = 'red';
+    x.style.textDecoration = 'none';
+}
+
+function semcor (x) {
+    x.style.color = 'black';
+}
